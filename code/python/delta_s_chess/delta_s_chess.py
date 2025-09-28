ab359208eb08b52c19c04605390bf7254b956d47
Delta_PrimeXII <deltaprimexii@gmail.com>
2025-09-28 18:23:30 +0200

    Started project (board and pieces display)

--- code/python/delta_s_chess/delta_s_chess.py
+++ code/python/delta_s_chess/delta_s_chess.py
@@ -1,3 +1,106 @@
-#Delta's chess
+#Delta's Chess
 
-print("Hello everyone, it's Delat_PrimeXII !")
+import pygame, sys
+from pygame.locals import *
+import time
+
+pygame.init()
+
+WIDTH = 738
+HEIGHT = 512
+
+fenetre = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Delta's Chess")
+
+font = pygame.font.Font(pygame.font.get_default_font(), 36)
+
+background = (14,15,19)
+
+fenetre.fill(background)
+pygame.display.flip()
+
+#==================================================
+class Board:
+    default_board = [
+        ["r","n","b","q","k","b","n","r"],
+        ["p","p","p","p","p","p","p","p"],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        ["p","p","p","p","p","p","p","p"],
+        ["r","n","b","q","k","b","n","r"]
+    ]
+    def __init__(self):
+        board = []
+        line = []
+        for y in range(8):
+            for x in range(8):
+                if Board.default_board[y][x] != " ":
+                    line.append(Case(x, y, Piece(x, y, y > 3, Board.default_board[y][x])))
+                else:
+                    line.append(Case(x, y))
+            board.append(line)
+            line = []
+        self.board = board
+    
+    def display(self):
+        color = True
+        for y in range(8):
+            for x in range(8):
+                if color:
+                    pygame.draw.rect(fenetre, (0, 0, 0), (x*64, y*64, 64, 64))
+                else:
+                    pygame.draw.rect(fenetre, (255, 255, 255), (x*64, y*64, 64, 64))
+                if self.board[y][x].piece:
+                    fenetre.blit(font.render(self.board[y][x].piece.race, True, (127,127,127)), dest=(x*64, y*64))
+                color = not color
+            color = not color
+
+#==================================================
+class Case:
+    def __init__(self, x:int, y:int, piece:"Piece"=None):
+        self.x = x
+        self.y = y
+        self.piece = piece
+    
+#==================================================
+class Piece:
+    def __init__(self, x:int, y:int, color:bool, race:str="p"):
+        self.x = x
+        self.y = y
+        self.color = color # False = black, True = white
+        self.race = race
+
+
+
+
+
+#--------------------------------------------------
+def security():
+    if event.type == pygame.QUIT:
+        pygame.display.quit()
+        sys.exit()
+#--------------------------------------------------
+
+#--------------------------------------------------
+
+#--------------------------------------------------
+
+#--------------------------------------------------
+board = Board()
+board.display()
+
+while True :
+
+    #================================#
+    for event in pygame.event.get(): #
+        security()                   #
+    #================================# 
+        if event.type == VIDEORESIZE:
+            WIDTH, HEIGHT = event.w, event.h
+            fenetre = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+    #================================#
+    fenetre.fill(background)
+    board.display()
+    pygame.display.flip()

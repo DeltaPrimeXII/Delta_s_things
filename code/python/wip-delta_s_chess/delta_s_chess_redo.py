6e4ece4aa2ec081e575ca02629c0f61279237d78
DeltaPrimeXII <deltaprimexii@gmail.com>
2026-04-08 14:09:59 +0200

    Started working on rendering with pyglet

--- code/python/wip-delta_s_chess/delta_s_chess_redo.py
+++ code/python/wip-delta_s_chess/delta_s_chess_redo.py
@@ -1,17 +1,46 @@
 #Delta's Chess
 
-# import pyglet
+import pyglet
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 from _libraries import keyboard as kb
 
-print("Let's do chessing")
+#==================================================
 
-key = kb.key
-while True:
-    kb.update_keys(key)
-    if key["l_click"].is_just_pressed():
-        print("click")
-    if key["r_click"].is_pressed():
-        break
+#Nearest-neighbor sampling by default
+pyglet.image.Texture.default_min_filter = pyglet.gl.GL_NEAREST
+pyglet.image.Texture.default_mag_filter = pyglet.gl.GL_NEAREST
+
+#==================================================
+
+class Game_Window:
+
+    def __init__(self):
+        self.window = pyglet.window.Window(width=1280, height=720, caption="Delta's Chess", resizable=True)
+        pyglet.gl.glClearColor(0.1, 0.1, 0.1, 1)
+        # self.sprite = pyglet.sprite.Sprite(img=pyglet.image.load('assets/drawings/space_8.png'), x=0, y=0)
+        # self.sprite.scale = 10
+
+        @self.window.event
+        def on_resize(width, height):
+            pass
+
+        #Each frame
+        @self.window.event
+        def on_draw():
+            self.window.clear()
+            # self.sprite.draw()
+
+        @self.window.event
+        def on_key_press(symbol, modifiers):
+            pass
+
+
+    def run(self):
+        pyglet.app.run(1/60)
+
+#==================================================
+
+game_window = Game_Window()
+game_window.run()
